98bc9a97420a5282eb7d14eccb64ba4abd84ccd9
MOHAMED <100794138+MohamedOUMERTOU@users.noreply.github.com>
2024-09-21 15:42:41 +0100

    add render.yaml

--- mysite/settings.py
+++ mysite/settings.py
@@ -138,8 +138,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'mycv' / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 #STATICFILES_DIRS = ['D:/projectpython/portfolio/cv/mysite/mycv/static']
